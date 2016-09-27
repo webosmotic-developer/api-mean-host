d21470770bf898505f31c2fb2fdd768469ccdefb
sweety-wo <sweety.webashlar@gmail.com>
2016-09-27 13:14:04 +0530

    Contact API added.

---
+++ server/api/contact/contact.controller.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var _ = require('lodash');
+var Contact = require('./contact.model');
+
+// Get list of contacts
+exports.index = function(req, res) {
+  Contact.find(function (err, contacts) {
+    if(err) { return handleError(res, err); }
+    return res.status(200).json(contacts);
+  });
+};
+
+// Get a single contact
+exports.show = function(req, res) {
+  Contact.findById(req.params.id, function (err, contact) {
+    if(err) { return handleError(res, err); }
+    if(!contact) { return res.status(404).send('Not Found'); }
+    return res.json(contact);
+  });
+};
+
+// Creates a new contact in the DB.
+exports.create = function(req, res) {
+  Contact.create(req.body, function(err, contact) {
+    if(err) { return handleError(res, err); }
+    return res.status(201).json(contact);
+  });
+};
+
+// Updates an existing contact in the DB.
+exports.update = function(req, res) {
+  if(req.body._id) { delete req.body._id; }
+  Contact.findById(req.params.id, function (err, contact) {
+    if (err) { return handleError(res, err); }
+    if(!contact) { return res.status(404).send('Not Found'); }
+    var updated = _.merge(contact, req.body);
+    updated.save(function (err) {
+      if (err) { return handleError(res, err); }
+      return res.status(200).json(contact);
+    });
+  });
+};
+
+// Deletes a contact from the DB.
+exports.destroy = function(req, res) {
+  Contact.findById(req.params.id, function (err, contact) {
+    if(err) { return handleError(res, err); }
+    if(!contact) { return res.status(404).send('Not Found'); }
+    contact.remove(function(err) {
+      if(err) { return handleError(res, err); }
+      return res.status(204).send('No Content');
+    });
+  });
+};
+
+function handleError(res, err) {
+  return res.status(500).send(err);
+}
--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,7 @@ var path = require('path');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/contacts', require('./api/contact'));
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
