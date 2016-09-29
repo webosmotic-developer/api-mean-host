ca7416bfc0c4667c34058b2b7e9e9d8caf9443d7
sweety-wo <sweety.webashlar@gmail.com>
2016-09-29 15:32:53 +0530

    Send user info in login API.

--- server/auth/local/index.js
+++ server/auth/local/index.js
@@ -6,15 +6,27 @@ var auth = require('../auth.service');
 
 var router = express.Router();
 
-router.post('/', function(req, res, next) {
-  passport.authenticate('local', function (err, user, info) {
-    var error = err || info;
-    if (error) return res.status(401).json(error);
-    if (!user) return res.status(404).json({message: 'Something went wrong, please try again.'});
+router.post('/', function (req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
+        var error = err || info;
+        if (error) return res.status(401).json(error);
+        if (!user) return res.status(404).json({message: 'Something went wrong, please try again.'});
 
-    var token = auth.signToken(user._id, user.role);
-    res.json({token: token, user:user});
-  })(req, res, next)
+        var cloneUser = clone(user);
+        delete cloneUser.hashedPassword;
+        delete cloneUser.salt;
+        var token = auth.signToken(user._id, user.role);
+        res.json({token: token, user: cloneUser});
+    })(req, res, next)
 });
+
+function clone(obj) {
+    if (null == obj || "object" != typeof obj) return obj;
+    var copy = obj.constructor();
+    for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+    }
+    return copy;
+}
 
 module.exports = router;
