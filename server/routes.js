adf3c6dcf3ca90e73b6c9fb5a9cfde39b9af182a
sweety-wo <sweety.webashlar@gmail.com>
2016-10-05 13:28:15 +0530

    allow origins for locals

--- server/routes.js
+++ server/routes.js
@@ -7,22 +7,30 @@
 var errors = require('./components/errors');
 var path = require('path');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  // Insert routes below
-  app.use('/api/contacts', require('./api/contact'));
-  app.use('/api/things', require('./api/thing'));
-  app.use('/api/users', require('./api/user'));
+    //CORS middleware
+    var allowCrossDomain = function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    };
+    app.use(allowCrossDomain);
 
-  app.use('/auth', require('./auth'));
-  
-  // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-   .get(errors[404]);
+    // Insert routes below
+    app.use('/api/contacts', require('./api/contact'));
+    app.use('/api/things', require('./api/thing'));
+    app.use('/api/users', require('./api/user'));
 
-  // All other routes should redirect to the index.html
-  app.route('/*')
-    .get(function(req, res) {
-      res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
-    });
+    app.use('/auth', require('./auth'));
+
+    // All undefined asset or api routes should return a 404
+    app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+        .get(errors[404]);
+
+    // All other routes should redirect to the index.html
+    app.route('/*')
+        .get(function (req, res) {
+            res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
+        });
 };
